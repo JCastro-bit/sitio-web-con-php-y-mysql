cae120e01a976a95a6bfb4ee00ba8f6a59048ce2
javier castro <javiercastro27@outlook.es>
2022-08-22 16:56:57 -0500

    Connection to database

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,10 +1,44 @@
 <?php include("../template/cabecera.php")?>
 <?php 
-
     $txtID=(isset($_POST['txtID']))?$_POST['txtID']:"";
     $txtNombre=(isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
-    $txtImagen=(isset($_FILES['txtImagen']['name']))?$_POST['txtImagen']['name']:"";
-    $accion=(isset($_POST['txtID']))?$_POST['txtID']:"";
+    $txtImagen=(isset($_FILES['txtImagen']['name']))?$_FILES['txtImagen']['name']:"";
+    $accion=(isset($_POST['accion']))?$_POST['accion']:"";
+
+    echo $txtID."<br/>";
+    echo $txtNombre."<br/>";
+    echo $txtImagen."<br/>";
+    echo $accion."<br/>";
+
+    $host="localhost";
+    $bd="sitio";
+    $usurio="root";
+    $contrasenia="";
+
+    //Conexion a base de datos
+    try {
+        $conexion= new PDO("mysql:host=$host;dbname=$bd",$usurio,$contrasenia);
+        if ($conexion){ echo "Conectado ... a sistema";}
+    } catch ( Exception $ex) {
+        echo $ex->getMessage();
+    }
+
+
+    switch($accion){
+
+        
+
+        case"Agregar":
+           // INSERT INTO `libros` (`id`, `nombre`, `imagen`) VALUES (NULL, 'Libro de Php', 'imagen.jpg');
+            echo"Presionado botón agregar";
+            break;
+        case"Modificar":
+            echo"Presionado botón Modificar";
+            break;
+        case"Cancelar":
+            echo"Presionado botón Cancelar";
+            break;
+    }
 ?>
 
 <div class="col-md-5">
@@ -29,9 +63,9 @@
                     <input type="file" class="form-control" name="txtImagen" id="txtImagen" placeholder="Subir imagen">
                 </div>
                 <div class="btn-group" role="group" aria-label=" ">
-                    <button type="submit" name="accion" value="Agregar"class="btn btn-success">Agregar</button>
-                    <button type="submit" name="accion" value="Modificar"class="btn btn-warning">Modificar</button>
-                    <button type="submit" name="accion" value="Cancelar"class="btn btn-info">Cancelar</button>
+                    <button type="submit" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                    <button type="submit" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                    <button type="submit" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
                 </div>
 
             </form>
